320d5b7f647bed82056fc8cf70ad1eb9e626db83
Pe5h4 <pesekf@email.cz>
2025-04-01 09:45:50 +0200

    Add webpack

---
+++ webpack/webpack.dev.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.common');
+
+const devConfig = {
+	mode: "development",
+	entry: path.resolve(__dirname, "../src/index.jsx"),
+	output: {
+		path: path.resolve(__dirname, "../dist"),
+		filename: "bundle.js",
+		publicPath: "/"
+	},
+	devtool: "inline-source-map",
+	devServer: {
+		static: path.resolve(__dirname, "../public"),
+		port: 3000,
+		historyApiFallback: true,
+		open: true,
+		hot: true
+	},
+	resolve: {
+		extensions: [".js", ".jsx"]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "../public/index.html")
+		})
+	]
+};
+
+// Merge common and dev configs
+module.exports = merge(commonConfig, devConfig);
